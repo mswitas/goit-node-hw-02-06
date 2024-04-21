5cb523b8137739f06c87d8b66eaca3a4976c5302
mswitas <mswitas@gmail.com>
2024-04-21 14:08:21 +0200

    add routs

--- models/contacts.js
+++ models/contacts.js
@@ -1,11 +1,12 @@
 const fs = require('fs/promises');
 const path = require('path');
+const nanoid = require('nanoid-esm');
 
 const contactsPath = path.join(__dirname, 'contacts.json');
 
 const listContacts = async () => {
   try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
+    const data = await fs.readFile(contactsPath);
     const contacts = JSON.parse(data);
     console.log('Contacts downloaded successfully');
     return contacts;
@@ -17,7 +18,7 @@ const listContacts = async () => {
 
 const getContactById = async (contactId) => {
   try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
+    const data = await fs.readFile(contactsPath);
     const contacts = JSON.parse(data);
     const contactById = contacts.find(contact => contact.id === contactId);
     console.log(`Contact with id: ${contactId} downloaded successfully`);
@@ -28,11 +29,71 @@ const getContactById = async (contactId) => {
   }
 }
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const newConstacts = contacts.filter(contact => contact.id !== contactId);
+    try {
+      await fs.writeFile(contactsPath, JSON.stringify(newConstacts));
+      return true;
+    } catch (error) {
+      console.error('Error writing contacts file: ', error);
+      throw error;
+    }
+  } catch (error) {
+    console.error('Error reading contacts file: ', error);
+    throw error;
+  }
+}
 
-const addContact = async (body) => {}
+const addContact = async ({name, email, phone}) => {
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  }
 
-const updateContact = async (contactId, body) => {}
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    contacts.push(newContact);
+    try {
+      await fs.writeFile(contactsPath, JSON.stringify(contacts));
+      return newContact;
+    } catch (error) {
+      console.error('Error writing contacts file: ', error);
+      throw error;
+    }
+  } catch (error) {
+    console.error('Error reading contacts file: ', error);
+    throw error;
+  }
+
+}
+
+const updateContact = async (contactId, body) => {
+  const contact = await getContactById(contactId);
+  const newContact = { ...contact, ...body };
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const newConstacts = contacts.map(
+      (contact) => (contact.id === contactId ? newContact : contact)
+    )
+    try {
+      await fs.writeFile(contactsPath, JSON.stringify(newConstacts));
+      return newContact;
+    } catch (error) {
+      console.error('Error writing contacts file: ', error)
+      throw error;
+    }
+  } catch (error) {
+    console.error('Error reading contacts file: ', error);
+    throw error;
+  }
+}
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const { listContacts, getContactById, removeContact, addContact, updateContact } = require('../../models/contacts.js');
-
+const Joi = require('joi');
 const router = express.Router()
+
+const userSchemaPOST = Joi.object({
+  name: Joi.string().min(5).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().min(9).required(),
+});
+
+const userSchemaPATCH = Joi.object({
+  name: Joi.string().min(5),
+  email: Joi.string().email(),
+  phone: Joi.string().min(9),
+})
 
 router.get('/', async (req, res, next) => {
   const contacts = await listContacts();
@@ -10,19 +22,74 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:contactId', async (req, res, next) => {
   const contact = await getContactById(req.params.contactId);
-  res.json(contact);
+  if (contact) {
+    return res.json(contact);
+  }
+  
+  res
+    .status(404)
+    .json({ message: 'Not found' });
+
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const body = req.body;
+    const { error } = userSchemaPOST.validate(body);
+    if (error) {
+      const validatingErrorMessage = error.details[0].message;
+      return res
+        .status(400)
+        .json({ message: `${validatingErrorMessage}` });
+    }
+
+    const addedContact = await addContact(body);
+    console.log('Contact added successfully');
+    res
+      .status(201)
+      .json(addedContact);
+  } catch (error) {
+    console.error('Error during adding contact');
+    next(error);
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contactId = req.params.contactId;
+  const contact = await getContactById(contactId);
+  
+  if (contact) {
+    await removeContact(contactId);
+    return res.json({ message: 'Contact deleted' });
+  } 
+    
+  res.status(404).json({ message: 'Not found' });
+
 })
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.patch('/:contactId', async (req, res, next) => {
+  const contactId = req.params.contactId;
+  const body = req.body;
+  const { error } = userSchemaPATCH.validate(body);
+  
+  if (error) {
+    const validatingErrorMessage = error.details[0].message;
+    return res
+        .status(400)
+        .json({ message: `${validatingErrorMessage}` });
+  }
+
+  const contact = await getContactById(contactId);
+
+  if (contact) {
+    const newContact = await updateContact(contactId, body);
+    return res
+      .status(200)
+      .json(newContact);
+  }
+
+  res.status(404).json({ message: 'Not found' });
+
 })
 
 module.exports = router
